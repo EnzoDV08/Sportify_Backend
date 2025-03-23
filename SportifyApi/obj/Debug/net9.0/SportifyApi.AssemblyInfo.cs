63aba8585c9df77be069305ce2bb81ddf2a17ac5
JoshuaDeKlerk <231207@virtualwindow.co.za>
2025-03-23 19:28:08 +0200

    Database connection

--- SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
+++ SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9629c78d1a161ca1654dcc44e97f80603af5b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
