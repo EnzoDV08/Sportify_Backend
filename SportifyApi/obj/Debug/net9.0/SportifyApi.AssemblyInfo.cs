adcd4f606ada5113caf7ef5d0f2d6e2d0533955e
Enzo De Vittorio <125486388+EnzoDV08@users.noreply.github.com>
2025-03-23 19:38:46 +0200

    new

--- SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
+++ SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb4338045592286be1f3fbf4630721db4bec61f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7932df46c392a023e3ff3a153f60e325ba996ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
