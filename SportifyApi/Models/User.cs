f201985dbf419c898f7106991d14284cf8287c92
Enzo De Vittorio <125486388+EnzoDV08@users.noreply.github.com>
2025-03-23 21:05:48 +0200

    Added user table, controller, Swagger, and Aiven DB setup

---
+++ SportifyApi/Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportifyApi.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [Column("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Column("password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Column("user_type")]
+        public string UserType { get; set; } = "user"; // user, admin, creator
+    }
+}
--- SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
+++ SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bf6d918765d3361355cf820499d996a770cf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cecde06922157261253d9d9cc139f8cba79a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
