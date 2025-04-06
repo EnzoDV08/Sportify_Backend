7931387988e0d6963f7c2acb6f575a8edcffe03f
Enzo De Vittorio <125486388+EnzoDV08@users.noreply.github.com>
2025-04-06 23:39:10 +0200

    Add achievement system: models, services, controllers, DTOs

---
+++ SportifyApi/Interfaces/IAchievementService.cs
@@ -0,0 +1,12 @@
+using SportifyApi.DTOs;
+
+namespace SportifyApi.Interfaces
+{
+    public interface IAchievementService
+    {
+        Task<IEnumerable<AchievementDto>> GetAllAchievementsAsync();
+        Task<bool> CreateAchievementAsync(AchievementDto dto);
+        Task<bool> AssignToUserAsync(UserAchievementDto dto);
+        Task<IEnumerable<AchievementDto>> GetUserAchievementsAsync(int userId);
+    }
+}
