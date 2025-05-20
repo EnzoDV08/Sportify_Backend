af98381ac92ed2181747d23326be25dfc3e554cd
ZanderBez <221205@virtualwindow.co.za>
2025-05-20 23:21:53 +0200

    Merge With Josh

--- SportifyApi/Controllers/UsersController.cs
+++ SportifyApi/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportifyApi.Data;
 using SportifyApi.DTOs;
 using SportifyApi.Interfaces;
+using SportifyApi.Models;
 
 namespace SportifyApi.Controllers
 {
@@ -9,20 +12,20 @@ namespace SportifyApi.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly AppDbContext _context;
 
-        public UsersController(IUserService userService)
+        public UsersController(IUserService userService, AppDbContext context)
         {
             _userService = userService;
+            _context = context;
         }
 
-        // GET: api/users
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
         {
             return Ok(await _userService.GetAllUsersAsync());
         }
 
-        // GET: api/users/5
         [HttpGet("{id}")]
         public async Task<ActionResult<UserDto>> GetUser(int id)
         {
@@ -30,7 +33,6 @@ namespace SportifyApi.Controllers
             return user == null ? NotFound() : Ok(user);
         }
 
-        // POST: api/users
         [HttpPost]
         public async Task<ActionResult<UserDto>> CreateUser(UserDto userDto)
         {
@@ -39,12 +41,10 @@ namespace SportifyApi.Controllers
                 return BadRequest("Password is required.");
             }
 
-            var createdUser = await _userService.CreateUserAsync(userDto, userDto.Password!); // The `!` tells C# "trust me it's not null"
+            var createdUser = await _userService.CreateUserAsync(userDto, userDto.Password!);
             return CreatedAtAction(nameof(GetUser), new { id = createdUser.UserId }, createdUser);
         }
 
-
-        // PUT: api/users/5
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, UserDto updatedUser)
         {
@@ -52,12 +52,32 @@ namespace SportifyApi.Controllers
             return success ? NoContent() : NotFound();
         }
 
-        // DELETE: api/users/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
             var success = await _userService.DeleteUserAsync(id);
             return success ? NoContent() : NotFound();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginRequest.Email);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.Password))
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+
+            var userDto = new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType
+            };
+
+            return Ok(userDto);
         }
     }
 }
