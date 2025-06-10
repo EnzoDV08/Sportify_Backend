cbc0b87e8f91a3cdf84967fc9439b3571323f28b
JoshuaDeKlerk <231207@virtualwindow.co.za>
2025-06-10 13:58:59 +0200

    Added Package

--- SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
+++ SportifyApi/obj/Debug/net9.0/SportifyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97dcff6df09eb9d2c4f312f0fd78f42f7fc61a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c140d2082ae722ecaac78b33af529e63f97602")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportifyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
